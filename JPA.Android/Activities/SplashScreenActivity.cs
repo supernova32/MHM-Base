1157a7526d5ce9494493cb9d42979d1ac0d5962d
Patricio Cano <suprnova32@gmail.com>
2014-01-27 11:30:28 +0100

    Removed AndHUD

--- JPA.Android/Activities/SplashScreenActivity.cs
+++ JPA.Android/Activities/SplashScreenActivity.cs
@@ -2,19 +2,24 @@ using System;
 using Android.App;
 using Android.OS;
 using PerpetualEngine.Storage;
-using AndroidHUD;
 using MHMBase;
+using Android.Widget;
+using Android.Views;
+using Android.Content.PM;
 
 namespace JPA.Android
 {
-	[Activity (Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]			
+	[Activity (Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait)]			
 	public class SplashScreenActivity : Activity
 	{
+		ProgressBar status;
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.SplashView);
+			status = FindViewById<ProgressBar> (Resource.Id.setup_bar);
+			status.Visibility = ViewStates.Visible;
 			SimpleStorage.SetContext(ApplicationContext);
-			AndHUD.Shared.Show(this, "Loading", -1,  MaskType.Clear);
 			var storage = SimpleStorage.EditGroup("preferences");
 			var value = storage.Get("companies_exist");
 			var cnHelper = ConnectivityHelper.Instance (this);
@@ -22,13 +27,18 @@ namespace JPA.Android
 				if (cnHelper.NetworkAvailable ()) {
 					UpdateCompanies (storage);
 				} else {
-					AndHUD.Shared.ShowError (this, "Network connection required", MaskType.Black, TimeSpan.FromSeconds (5));
+					var builder = new AlertDialog.Builder (this);
+					builder.SetMessage (Resource.String.network_needed).SetTitle (Resource.String.no_network);
+					builder.SetPositiveButton (Resource.String.ok, delegate {
+						Finish ();
+					});
+					var alert = builder.Create ();
+					alert.Show ();
 				}
 			} else {
 				if (cnHelper.NetworkAvailable ()) {
 					UpdateCompanies (storage);
 				} else {
-					AndHUD.Shared.Dismiss (this);
 					StartActivity (typeof(MainActivity));
 				}
 			}
@@ -39,9 +49,16 @@ namespace JPA.Android
 			cp.PopulateCompaniesDB (success => RunOnUiThread (() => {
 				if (success)
 					storage.Put ("companies_exist", "True");
-				AndHUD.Shared.Dismiss (this);
 				StartActivity (typeof(MainActivity));
-			}), DatabaseHelper.Instance.Connection);		
+			}), state => RunOnUiThread (() => {
+				var builder = new AlertDialog.Builder (this);
+				builder.SetMessage (Resource.String.network_needed).SetTitle (Resource.String.no_network);
+				builder.SetPositiveButton (Resource.String.ok, delegate {
+					Finish ();
+				});
+				var alert = builder.Create ();
+				alert.Show ();
+			}));		
 		}
 	}
 }
