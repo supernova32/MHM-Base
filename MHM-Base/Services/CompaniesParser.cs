58e5e1edaabdb2ab22ef21656b8f94415d2b8eaf
Patricio Cano <suprnova32@gmail.com>
2014-01-27 11:38:07 +0100

    Added error handling and background thread

--- MHM-Base/Services/CompaniesParser.cs
+++ MHM-Base/Services/CompaniesParser.cs
@@ -11,36 +11,42 @@ namespace MHMBase
 	public class CompaniesParser
 	{
 		const string _baseUrl = "https://insomniware.com/companies.xml";
-		public bool PopulateCompaniesDB(Action<bool> callback, SQLiteConnection db) {
+		public bool PopulateCompaniesDB(Action<bool> callback, Action<bool> error) {
+			var db = DatabaseHelper.Instance.Connection;
 			db.CreateTable<Company>();
 			var client = new WebClient ();
 			var success = false;
 			client.DownloadStringCompleted += (sender, args) => {
-				var companies = XDocument
-					.Parse(args.Result)
-					.Descendants("company").Select(item => new Company {
-						Name = item.Element("name").Value,
-						FullName = item.Element("full_name").Value,
-						IconPath = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Personal), item.Element("name").Value+".png"),
-						IconUrl = item.Element("icon").Value
-					}).ToList();
+				try {
+					var companies = XDocument
+						.Parse(args.Result)
+						.Descendants("company").Select(item => new Company {
+							Name = item.Element("name").Value,
+							FullName = item.Element("full_name").Value,
+							IconPath = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Personal), item.Element("name").Value+".png"),
+							IconUrl = item.Element("icon").Value
+						}).ToList();
 
-				foreach (var c in companies) {
-					try {
-						db.Insert (c);
-						var webClient = new WebClient();
-						var url = new Uri (c.IconUrl);
-						var image_bytes = webClient.DownloadData (url);
-						string documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal);	
-						string localPath = Path.Combine (documentsPath, c.Name+".png");
-						Console.WriteLine("localPath:"+localPath);
-						File.WriteAllBytes (localPath, image_bytes);		
-					} catch (SQLiteException) {
-						Console.WriteLine ("CompaniesParser: Duplicate detected");					
+					foreach (var c in companies) {
+						try {
+							db.Insert (c);
+							var webClient = new WebClient();
+							var url = new Uri (c.IconUrl);
+							var image_bytes = webClient.DownloadData (url);
+							string documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal);	
+							string localPath = Path.Combine (documentsPath, c.Name+".png");
+							Console.WriteLine("localPath:"+localPath);
+							File.WriteAllBytes (localPath, image_bytes);		
+						} catch (SQLiteException) {
+							Console.WriteLine ("CompaniesParser: Duplicate detected");					
+						}
 					}
+					success = true;
+					callback(success);
+				} catch (System.Reflection.TargetInvocationException) {
+					Console.WriteLine ("TargetInvocationException");
+					error (true);				
 				}
-				success = true;
-				callback(success);
 
 			};
 			client.DownloadStringAsync (new Uri (_baseUrl));
--- MHM-Base/Services/PublicationsParser.cs
+++ MHM-Base/Services/PublicationsParser.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json.Linq;
 using SQLite;
 using System.Text;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace MHMBase
 {
@@ -15,32 +16,39 @@ namespace MHMBase
 		const string _baseUrl = "https://insomniware.com/publications.xml";
 		readonly SQLiteConnection db = DatabaseHelper.Instance.Connection;
 
-		public void UpdatePublications(Action<IList<Publication>> callback) {
+		public void UpdatePublications(Action<IList<Publication>> callback, Action<bool> error) {
 			db.CreateTable<Publication>();
 			var client = new WebClient ();
 			client.DownloadStringCompleted += (sender, args) => {
-				var pubs = XDocument
-					.Parse(args.Result)
-					.Descendants("publication").Select(item => new Publication {
-						RemoteId = item.Element("id").Value,
-						Title = item.Element("title").Value,
-						Company = item.Element("company").Value,
-						FullDescription = item.Element("full_description").Value,
-						ShortDescription = item.Element("short_description").Value,
-						Link = item.Element("link").Value
-					}).ToList();
-
-				foreach (var p in pubs) {
-					try {
-						db.Insert (p);
-					} catch (SQLiteException){
-						Console.WriteLine ("PublicationParser: Duplicate detected");
-					}				
+				try {
+					var pubs = XDocument
+						.Parse(args.Result)
+						.Descendants("publication").Select(item => new Publication {
+							RemoteId = item.Element("id").Value,
+							Title = item.Element("title").Value,
+							Company = item.Element("company").Value,
+							FullDescription = item.Element("full_description").Value,
+							ShortDescription = item.Element("short_description").Value,
+							Link = item.Element("link").Value
+						}).ToList();
+					foreach (var p in pubs) {
+						try {
+							db.Insert (p);
+						} catch (SQLiteException){
+							Console.WriteLine ("PublicationParser: Duplicate detected");
+						}				
+					}
+				} catch (System.Reflection.TargetInvocationException) {
+					Console.WriteLine ("TargetInvocationException");
+					error (true);			
 				}
+
+
 				callback(Publications);
 			};
 
 			client.DownloadStringAsync (new Uri (_baseUrl));
+			
 		}
 
 		public void LocalSearch (Action<IList<Publication>> callback, string query) {
@@ -48,13 +56,51 @@ namespace MHMBase
 			callback (publications);		
 		}
 
-		public void SendSearchParameters (Action<IList<Publication>> callback, string parameters) {
-			var http = (HttpWebRequest)WebRequest.Create(new Uri("http://192.168.2.1:3000/api/v1/publications"));
+		public void SendSearchParameters (Action<IList<Publication>> callback, string parameters, Action<bool> error) {
+			var http = (HttpWebRequest)WebRequest.Create (new Uri ("http://192.168.10.149:3000/api/v1/publications"));
 			http.Accept = "application/json";
 			http.ContentType = "application/json";
 			http.Method = "POST";
 
 			var jObject = new JObject (new JProperty ("query", parameters));
+			var encoding = new UTF8Encoding ();
+			Byte[] bytes = encoding.GetBytes (jObject.ToString ());
+			IList<Publication> publications = new List<Publication> ();
+			Task.Factory.StartNew (() =>  {
+				var newStream = http.GetRequestStream ();
+				newStream.Write (bytes, 0, bytes.Length);
+				newStream.Close ();
+				var response = http.GetResponse ();
+				var stream = response.GetResponseStream ();
+				var sr = new StreamReader (stream);
+				var content = sr.ReadToEnd ();
+				var resultJSON = JObject.Parse (content);
+				publications = resultJSON ["publications"].Select (item => new Publication {
+					RemoteId = item ["id"].ToString (),
+					Title = (string)item ["title"],
+					Company = (string)item ["company"],
+					FullDescription = (string)item ["full_description"],
+					ShortDescription = (string)item ["short_description"],
+					Link = (string)item ["link"]
+				}).ToList ();
+			}).ContinueWith (state =>  {
+				Console.WriteLine ("State of exec: " + state.Exception);
+				if (state.Status == TaskStatus.Faulted) {
+					error (true);
+				}
+				else {
+					callback (publications);
+				}
+			});
+		}
+
+		public Publication GetRemotePublication (int remoteId) {
+			var http = (HttpWebRequest)WebRequest.Create(new Uri("http://192.168.2.1:3000/api/v1/publications"));
+			http.Accept = "application/json";
+			http.ContentType = "application/json";
+			http.Method = "POST";
+
+			var jObject = new JObject (new JProperty ("publication_id", remoteId));
 			var encoding = new UTF8Encoding();
 			Byte[] bytes = encoding.GetBytes(jObject.ToString());
 
@@ -67,23 +113,18 @@ namespace MHMBase
 			var stream = response.GetResponseStream();
 			var sr = new StreamReader (stream);
 			var content = sr.ReadToEnd();
-			var resultJSON = JObject.Parse (content);
-			IList<Publication> publications = resultJSON["publications"].Select(item => new Publication	{
+			var item = JObject.Parse (content);
+			var publication = new Publication {
 				RemoteId = item["id"].ToString(),
 				Title = (string)item["title"],
 				Company = (string)item["company"],
 				FullDescription = (string)item["full_description"],
 				ShortDescription = (string)item["short_description"],
 				Link = (string)item["link"]					
-			}).ToList();
-			callback(publications);
-		}
+			};
 
-//		public Publication GetRemotePublication (int remoteId) {
-//			var client = new WebClient();
-//			client.Headers.Add("Content-Type", "application/json");
-//		
-//		}
+			return publication;	
+		}
 
 		public IList<Publication> Publications {
 			get {
