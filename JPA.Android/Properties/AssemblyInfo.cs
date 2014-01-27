cd96e650fd89535c78cbd7ab3156e50d78275ed0
Patricio Cano <suprnova32@gmail.com>
2014-01-27 11:35:29 +0100

    Added GCM permissions

--- JPA.Android/Properties/AssemblyInfo.cs
+++ JPA.Android/Properties/AssemblyInfo.cs
@@ -12,6 +12,14 @@ using Android.App;
 [assembly: AssemblyCopyright ("pato")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
+// This will prevent other apps on the device from receiving GCM messages for this app
+// It is crucial that the package name does not start with an uppercase letter - this is forbidden by Android.
+[assembly: Permission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: UsesPermission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: MetaData (("com.google.android.gms.version"), Value = "@integer/google_play_services_version")]
+
+// Gives the app permission to register and receive messages.
+[assembly: UsesPermission(Name = "com.google.android.c2dm.permission.RECEIVE")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
